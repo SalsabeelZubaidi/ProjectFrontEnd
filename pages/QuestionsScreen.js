e9c176fd359893429ec065727aad61ac14bd2505
SalsabeelZubaidi <158805325+SalsabeelZubaidi@users.noreply.github.com>
2025-04-12 21:24:28 +0300

    post signup +Questions screens

--- pages/QuestionsScreen.js
+++ pages/QuestionsScreen.js
@@ -1,189 +1,245 @@
 import React, { useState } from 'react';
-import { Text, TextInput, StyleSheet, View, Button, TouchableOpacity } from 'react-native';
+import 
+{ Text, TextInput, StyleSheet, View, Button, TouchableOpacity, Animated, useWindowDimensions, ScrollView,useAnimatedValue,ImageBackground, Dimensions} 
+from 'react-native';
 import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
 import { useLanguage } from '../LanguageContext';
 import SwitchLanguageBtn from '../components/SwitchLanguageBtn';
 import { I18nManager } from 'react-native';
+import LottieView from 'lottie-react-native';
+import LogoImage from '../components/LogoImage';
+
+const { width, height } = Dimensions.get('window');
 
 const QuestionsScreen = ({ route, navigation }) => {
   const { translation } = route.params;
   const { language, switchLanguage } = useLanguage(); 
 
- 
+  const scrollX = useAnimatedValue(0);
+  const {width: windowWidth} = useWindowDimensions();
+    
   const [cigsCount, setCigsCount] = useState('');
   const [cigsCountPerPack, setCigsCountPerPack] = useState('');
   const [packCost, setPackCost] = useState('');
   const [quitTime, setQuitTime] = useState('');
-  
+
+  const [isLastPage, setIsLastPage]= useState(false);
+  const [isFirstPage, setIsFirstPage]= useState(false);
+
   const forgetPassPage = () => {
       //navigation.navigate('ForgetPass');
   };
 
+  const loadingScreen=()=>{
+    navigation.navigate('LoadingScreen');
+  }
 
- 
+  const handleScroll = ({ layoutMeasurement, contentOffset, contentSize })=> {
+    if (layoutMeasurement && contentOffset && contentSize) {
+      const paddingToRight = 20;
+      if (layoutMeasurement.width + contentOffset.x >= contentSize.width - paddingToRight) {
+        setIsLastPage(true);
+      } else {
+        setIsLastPage(false);
+      }
+      if (contentOffset.x < paddingToRight) {
+        setIsFirstPage(true);
+      } else {
+        setIsFirstPage(false);
+      }
+    }
+  };
+  
+
   return (
-    <SafeAreaProvider>
-      <SafeAreaView style={styles.signup}>
+    <SafeAreaProvider style={{backgroundColor:'white' }}>
+      <SafeAreaView style={styles.container}>
+      <>
+      <View style={styles.bg}>
+         <LottieView style={{flex: 1 , opacity:.35}} source={require('../assets/bg4.json')} autoPlay loop speed={.2} ></LottieView>
+         
+      </View>
+      <LogoImage/>
+      <SwitchLanguageBtn switchLanguage={() =>{ switchLanguage(language === 'ar' ? 'en' : 'ar') }} />
 
-        <SwitchLanguageBtn switchLanguage={() =>{ switchLanguage(language === 'ar' ? 'en' : 'ar') }} />
-      
-
-        <View style={styles.signupForm}>
+      {/* {
+        isFirstPage &&(
+          <Text style={styles.header}> {translation('Qscreen')}</Text>
+        )
+      }
+       */}
         
-              <View>
-                <Text style={{fontWeight:'500'}}>{translation('cigsCount')}</Text>
-                <TextInput
-                  style={styles.input}
-                  onChangeText={setCigsCount}
-                  value={cigsCount}
-                  inputMode="decimal"
-                />
-              </View>
-              <View>
-                <Text style={{fontWeight:'500'}}>{translation('cigsCountPerPack')}</Text>
-                <TextInput
-                  style={styles.input}
-                  onChangeText={setCigsCountPerPack}
-                  value={cigsCountPerPack}
-                  inputMode="decimal"
-                />
-              </View>
-              <View>
-                <Text style={{fontWeight:'500'}}>{translation('packCost')}</Text>
-                <TextInput
-                  style={styles.input}
-                  onChangeText={setPackCost}
-                  value={packCost}
-                  inputMode="decimal"
-                />
-                </View>
-              <View>
-                <Text style={{fontWeight:'500'}}>{translation('quitTime')}</Text>
-                <TextInput
-                  style={styles.input}
-                  onChangeText={setQuitTime}
-                  value={quitTime}
-                  inputMode="decimal"
-                />
-              </View>
+        <View style={styles.scrollContainer}>
+          <ScrollView 
+            horizontal={true}
+            pagingEnabled={true} 
+            showsHorizontalScrollIndicator={false}
+            onScroll={Animated.event(
+              [{ nativeEvent: { contentOffset: { x: scrollX } } }],
+              {
+                useNativeDriver: false,
+                listener: (e) => handleScroll(e.nativeEvent),
+              }
+            )} 
+            scrollEventThrottle={16}>
+            {[
+                { key: 'cigsCount', label: translation('cigsCount'), value: cigsCount, setValue: setCigsCount },
+                { key: 'cigsCountPerPack', label: translation('cigsCountPerPack'), value: cigsCountPerPack, setValue: setCigsCountPerPack },
+                { key: 'packCost', label: translation('packCost'), value: packCost, setValue: setPackCost },
+                { key: 'quitTime', label: translation('quitTime'), value: quitTime, setValue: setQuitTime },
+                ].map((question, index) => (
+                    <View key={question.key} style={[styles.card, { width: 390 }]}>
+                    <Text style={styles.QuestionText}>{question.label}</Text>
+                    <TextInput
+                        style={styles.input}
+                        onChangeText={question.setValue}
+                        value={question.value}
+                        keyboardType='number-pad'
+                        placeholder="e.g., 10"
+                        />
+                        
+                        
+                    </View>
+                ))}
+            </ScrollView>
+            
+            <View style={styles.indicatorContainer}>
+                {Array(4).fill(0).map((_, index) => {
+                    const width = scrollX.interpolate({
+                    inputRange: [
+                        windowWidth * (index - 1),
+                        windowWidth * index,
+                        windowWidth * (index + 1),
+                    ],
+                    outputRange: [8, 16, 8],
+                    extrapolate: 'clamp',
+                    });
+
+                    return <Animated.View key={index} style={[styles.normalDot, { width }]} />;
+                })}
             </View>
 
-          <View style={{ borderRadius: 10, overflow: 'hidden', marginTop: 10 }}>
-            <Button
-              //onPress={accessApp}
-              title={'Btn'}
-              color="#6b8a6b"
-            />
-          </View>
+            
+
+          
+        </View>
+
+        {
+          isLastPage && (
+            <TouchableOpacity style={styles.btn} onPress={loadingScreen}>
+              <Text style={styles.btnText}> {translation('getStartedBtn')} </Text>
+            </TouchableOpacity>    
+          
+          )
+        }
+      </>
       </SafeAreaView>
     </SafeAreaProvider>
+    
   );
 };
 
-const isRtl = I18nManager.isRTL;
-
 const styles = StyleSheet.create({
-  activeTabText:{
-    fontWeight:'900'
-  },
-  signupTabItem: {
-    borderRadius: 5,
-    opacity: 0.45
-  },
-
-  signup: {
+  container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'white',
-    width: '100%',
-    fontSize: 11,
-    color: 'black',
+    justifyContent: 'center',
   },
-  signupHeader: {
-    textAlign: 'center',
+  scrollContainer: {
+    height: 300,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  signupTitle: {
-    fontSize: 25,
-    fontWeight: 'bold',
-    textAlign: 'center',
+  bg: {
+    height: 900,
+    position: 'absolute',
+    width:900  
   },
-  signupSubTitle: {
-    color: '#b4b4b4',
-    fontSize: 14,
-    textAlign: 'center',
-  },
-  signupTabs: {
-    display: 'flex',
-    flexDirection: isRtl ? 'row-reverse' : 'row',
-    justifyContent: 'space-evenly',
-    backgroundColor: '#f5f6f9',
-    fontWeight: 'bold',
-    marginTop: 3,
-    padding: 10,
-    borderRadius: 10,
-    marginBottom: 30,
-    marginTop: 15,
-    gap: 80,
-    paddingLeft: 70,
-    paddingRight: 70,
-    height: 'fit-content',
-  },
- 
-  signupForm: {
-    marginTop: 4
+  // header:{
+  //   // fontSize:17,
+  //   // padding:23,
+  //   // textAlign:'center',
+  //   // borderRadius:30,
+  //   // backgroundColor:'#F8FAFC',
+  //   backgroundColor: 'white',
+  //   margin:13,
+  //   borderRadius: 16,
+  //   borderColor:'white',
+  //   padding: 20,
+  //   shadowColor: '#000',
+  //   shadowOpacity: .05,
+  //   shadowRadius: 10,
+  //   elevation: .5,
+  //   fontSize: 18,
+  //   color: '#333',
+  //   textAlign: 'left',
+  //   fontFamily:'Poppins_700Bold',
+  //   color:'#3F4F44' 
+  // },
+  card: {
+    display:'flex', 
+    alignItems: 'center',
+    justifyContent:'center',
+    width:300,
+    marginLeft:13
   },
 
-  input: {
-    borderWidth: 1,
-    borderColor: '#c4c4c4',
-    padding: 10,
-    width: 300,
-    borderRadius: 10,
-    marginBottom: 10,
-    marginTop:9,
-    textAlign: isRtl ? 'right' : 'left',
+  QuestionText: {
+    fontSize: 22,
+    color:'black',
+    textAlign:'center',
+    justifyContent:'center',
+    marginLeft:10
   },
-  inputContainer: {
-    marginBottom: 10,
+  normalDot: {
+    height: 8,
+    width: 8,
+    borderRadius: 4,
+    backgroundColor: 'black',
+    marginHorizontal: 4,
+    opacity:.5,
+    marginBottom:30
   },
-  dropdownContainer: {
-    marginBottom: 20,
+  indicatorContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+
   },
-  dropdownButtonStyle: {
-    width: 250,
-    backgroundColor: '#f0f0f0',
-    borderRadius: 5,
-  },
-  dropdownButtonTxtStyle: {
-    color: '#333',
-  },
-  dropdownMenuStyle: {
+  input:{
     backgroundColor: '#fff',
+    borderRadius: 12,
+    paddingHorizontal: 20,
+    borderWidth: 1,
+    borderColor: '#ddd',
+    fontSize: 18,
+    width: '30%',
+    textAlign: 'center',
+    shadowColor: '#000',
+    shadowOpacity: 0.05,
+    shadowRadius: 4,
+    elevation: 3,
+    marginTop:15,
+    fontFamily:'Poppins_600SemiBold'
   },
+  btn: {
+    padding:10,
+    borderRadius:12,
+    backgroundColor:'#F8FAFC',
+    marginLeft:15,
+    marginTop:12,
+    shadowColor: '#000',
+    shadowOpacity: 0.05,
+    shadowRadius: 4,
+    elevation: 3,
+    
+  },
+  btnText:{
+    fontFamily:'Poppins_700Bold',
+    fontSize:20,
+    }
 });
 
-const pickerSelectStyles = StyleSheet.create({
-  inputIOS: {
-    fontSize: 16,
-    paddingVertical: 10,
-    paddingHorizontal: 12,
-    borderWidth: 1,
-    borderColor: 'gray',
-    borderRadius: 4,
-    color: 'black',
-    paddingRight: 30,
-  },
-  inputAndroid: {
-    fontSize: 16,
-    paddingVertical: 10,
-    paddingHorizontal: 12,
-    borderWidth: 1,
-    borderColor: 'gray',
-    borderRadius: 4,
-    color: 'black',
-    paddingRight: 30,
-  },
-});
+
 
 export default QuestionsScreen;
--- pages/SignUpInfo.js
+++ pages/SignUpInfo.js
@@ -3,6 +3,12 @@ import { Text, TextInput, StyleSheet, View, Button, TouchableOpacity } from 'rea
 import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
 import { useLanguage } from '../LanguageContext';
 import SwitchLanguageBtn from '../components/SwitchLanguageBtn';
+import LottieView from 'lottie-react-native';
+import LogoImage from '../components/LogoImage';
+
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import { faCircleChevronRight } from '@fortawesome/free-solid-svg-icons'
+
 
 const SignUpInfo = ({ route, navigation }) => {
   const { translation } = route.params;
@@ -15,33 +21,45 @@ const SignUpInfo = ({ route, navigation }) => {
   };
 
   return (
-    <SafeAreaProvider style={{backgroundColor: 'white'}} >
-      <SafeAreaView>
-
-        <SwitchLanguageBtn switchLanguage={() =>{ switchLanguage(language === 'ar' ? 'en' : 'ar') }} />
-
-        <View style={styles.text}>
-        <Text style= {{fontSize:20 , textAlign:'center'}}> {translation('welcomeMsg')}</Text>
+    <SafeAreaProvider style={{backgroundColor:'white'}}>
+     <LogoImage/>
+      <SafeAreaView style={styles.container}>
+      <SwitchLanguageBtn switchLanguage={() =>{ switchLanguage(language === 'ar' ? 'en' : 'ar') }} />
+          <View style={styles.lottie}>
+            <LottieView style={{flex: 1}} source={require('../assets/bubblesBG.json')} autoPlay loop></LottieView>
+          </View>
+          <View style={styles.text}>
+            <Text style= {{fontSize:20 , textAlign:'center',marginBottom:20}}> {translation('welcomeMsg')} </Text>
+          </View>  
+        <View style={{ borderRadius: 30, overflow: 'hidden', width:100 , alignSelf:'center'}}>
+          <TouchableOpacity onPress={QuestionsScreen}> 
+             <FontAwesomeIcon icon={faCircleChevronRight} size={39} style={styles.icon}/>
+          </TouchableOpacity>
         </View>
-
-
-        <View style={{ borderRadius: 10, overflow: 'hidden', marginTop: 16 , width:100 , alignSelf:'center'}}>
-          <Button style={styles.btn}
-          onPress={QuestionsScreen}
-          title={translation('getStartedBtn')}
-          color="#6b8a6b"
-          />
-        </View>
+        
       </SafeAreaView>
     </SafeAreaProvider>
   );
 };
 
 
-const styles = StyleSheet.create({
-  text:{
-    marginTop:180
-  } , 
+const styles = StyleSheet.create({ 
+  container: {
+    flex: 1,
+    justifyContent:'center',
+    fontFamily:'Quicksand_600SemiBold'
+
+  },
+  lottie:{
+    height:300,
+    marginTop:0,
+
+  },
+  icon:{
+    alignSelf:'center',
+    color:'#6b8a6b',
+  }
+  
   
 });
 
