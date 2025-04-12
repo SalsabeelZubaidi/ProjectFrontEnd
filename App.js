159b9559721e985ee385f2def615366dd8b55f1c
SalsabeelZubaidi <158805325+SalsabeelZubaidi@users.noreply.github.com>
2025-04-12 21:20:53 +0300

    updated packages

--- App.js
+++ App.js
@@ -1,21 +1,62 @@
-import { StyleSheet } from 'react-native';
+import React, { useEffect } from 'react';
+import { StyleSheet, ActivityIndicator, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { useTranslation } from 'react-i18next';
-import { useState } from "react";
-import { SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
+import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
+import { useFonts } from 'expo-font';
+import { Poppins_400Regular } from '@expo-google-fonts/poppins/400Regular';
+import { Poppins_600SemiBold } from '@expo-google-fonts/poppins/600SemiBold';
+import { Poppins_700Bold } from '@expo-google-fonts/poppins/700Bold';
 import SignUp from './pages/SignUp';
 import SignUpInfo from './pages/SignUpInfo';
 import ForgetPass from './pages/ForgetPass';
 import SetPass from './pages/SetPass';
 import QuestionsScreen from './pages/QuestionsScreen';
+import SplashScreen from './pages/SplashScreen';
+import LoadingScreen from './pages/LoadingScreen';
+import Plan from './pages/Plan';
+
+
+
 
 import { LanguageProvider, useLanguage } from './LanguageContext'; 
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-  
+
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_600SemiBold, 
+    Poppins_700Bold 
+  });
+
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      const oldRender = Text.render;
+      Text.render = function (...args) {
+        const origin = oldRender.call(this, ...args);
+        return React.cloneElement(origin, {
+          style: [{ fontFamily: 'Poppins_600SemiBold' }, origin.props.style],
+        });
+      };
+    }
+  }, [fontsLoaded]);
+
+  //Show loading while font is loading
+  if (!fontsLoaded) {
+    return (
+      <SafeAreaProvider>
+        <SafeAreaView style={styles.container}>
+          <ActivityIndicator size="large" color="#0000ff" />
+          <Text>Loading fonts...</Text>
+        </SafeAreaView>
+      </SafeAreaProvider>
+    );
+  }
+
   return (
     <LanguageProvider>
       <SafeAreaProvider>
@@ -23,6 +64,7 @@ export default function App() {
       </SafeAreaProvider>
     </LanguageProvider>
   );
+  
 }
 
 
@@ -30,13 +72,13 @@ function AppNavigation() {
   const { t, i18n } = useTranslation();
   
   return (
-    <SafeAreaView style={styles.container}>
-      
-      <NavigationContainer>
+    <SafeAreaView style={styles.container}>  
+      <NavigationContainer style={{backgroundColor:'white'}}>
         <Stack.Navigator
-          initialRouteName="SignUp"
+          initialRouteName="SplashScreen"
           screenOptions={{
             headerShown: false,
+            contentStyle: { paddingLeft: 0, paddingRight: 0 }
           }}
         >
           <Stack.Screen
@@ -62,6 +104,21 @@ function AppNavigation() {
           <Stack.Screen
             name="QuestionsScreen"
             component={QuestionsScreen}
+            initialParams={{ translation: t }}
+          />
+          <Stack.Screen
+            name="SplashScreen"
+            component={SplashScreen}
+            initialParams={{ translation: t }}
+          />
+          <Stack.Screen
+            name="LoadingScreen"
+            component={LoadingScreen}
+            initialParams={{ translation: t }}
+          />
+          <Stack.Screen
+            name="Plan"
+            component={Plan}
             initialParams={{ translation: t }}
           />
         </Stack.Navigator>
@@ -110,10 +167,24 @@ function AppNavigation() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "white",
+    backgroundColor: 'white',
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
-    paddingHorizontal:20
+    paddingLeft:0,
+    paddingRight:0,
+    fontFamily:'Poppins_500Medium'
+  },
+  regular: {
+    fontFamily: "Poppins_400Regular",
+    fontSize: 20,
+  },
+  medium: {
+    fontFamily: "Poppins_600SemiBold",
+    fontSize: 20,
+  },
+  bold: {
+    fontFamily: "Poppins_700Bold",
+    fontSize: 20,
   },
 });
